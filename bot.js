87a15cea013a576b92b4f0e5fde25ef4e70a6bfc
MONTIEL <3604098@ppti-14-503-10.ufr-info-p6.jussieu.fr>
2017-05-04 12:09:11 +0200

    make it better

--- bot.js
+++ bot.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+res.send('hello world');
+});
+
+
+
 client.on('ready', () => {
 console.log('I am ready!');
 });
@@ -14,3 +23,4 @@ client.on('presenceUpdate', function(oldMember, newMember) {
 console.log(oldMember.presence, '=>', newMember.presence);
 });
 client.login(process.env.DISCORD_TOKEN);
+app.listen(process.env.PORT || 5000);
